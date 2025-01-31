dbce4d18450cbd421269cc6ef866765d9023e913
jxxngin <wwd0203@daum.net>
2025-01-31 15:15:14 +0900

    [feat] add and test findBySubjectAndContent

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -57,4 +57,12 @@ class SbbApplicationTests {
 		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
 		assertEquals(1, q.getId());
 	}
+
+	@Test
+	@DisplayName("findBySubjectAndContent")
+	void t5() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+		assertEquals(1, q.getId());
+	}
 }
