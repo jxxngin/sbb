52817b1983d50b6184b20be50c136ef08cc4afb0
jxxngin <wwd0203@daum.net>
2025-01-31 15:10:09 +0900

    [test] findById

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,10 +1,12 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,6 +16,7 @@ class SbbApplicationTests {
 	private QuestionRepository questionRepository;
 
 //	@Test
+//	@DisplayName("insert")
 //	void t1() {
 //		Question q1 = new Question();
 //		q1.setSubject("sbb가 무엇인가요?");
@@ -29,11 +32,22 @@ class SbbApplicationTests {
 //	}
 
 	@Test
+	@DisplayName("findAll")
 	void t2() {
 		List<Question> all = this.questionRepository.findAll();
 		assertEquals(2, all.size());
 
 		Question q = all.get(0);
 		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@DisplayName("findById")
+	void t3() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if(oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
 	}
 }
