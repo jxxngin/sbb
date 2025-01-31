2b6dc433194ce9b9c58abf96370b7528ef63f165
jxxngin <wwd0203@daum.net>
2025-01-31 15:22:03 +0900

    [test] delete

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -85,4 +85,16 @@ class SbbApplicationTests {
 		q.setSubject("수정된 제목");
 		this.questionRepository.save(q);
 	}
+
+	@Test
+	@DisplayName("delete")
+	void t8() {
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
 }
