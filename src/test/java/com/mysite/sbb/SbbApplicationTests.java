9b38dce993307bdf42c89f6784cec5c10118d597
jxxngin <wwd0203@daum.net>
2025-02-03 14:41:14 +0900

    [feat] add paging

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,149 +1,22 @@
 package com.mysite.sbb;
 
-import com.mysite.sbb.answer.Answer;
-import com.mysite.sbb.answer.AnswerRepository;
-import com.mysite.sbb.question.Question;
-import com.mysite.sbb.question.QuestionRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.DisplayName;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-	private QuestionRepository questionRepository;
 
 	@Autowired
-	private AnswerRepository answerRepository;
-
-//	@Test
-//	@DisplayName("insert Q")
-//	void t1() {
-//		Question q1 = new Question();
-//		q1.setSubject("sbb가 무엇인가요?");
-//		q1.setContent("sbb에 대해서 알고 싶습니다.");
-//		q1.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q1);  // 첫번째 질문 저장
-//
-//		Question q2 = new Question();
-//		q2.setSubject("스프링부트 모델 질문입니다.");
-//		q2.setContent("id는 자동으로 생성되나요?");
-//		q2.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q2);  // 두번째 질문 저장
-//	}
+	private QuestionService questionService;
 
 	@Test
-	@DisplayName("findAll Q")
-	void t2() {
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
-
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
-	@Test
-	@DisplayName("findById Q")
-	void t3() {
-		Optional<Question> oq = this.questionRepository.findById(1);
-		if(oq.isPresent()) {
-			Question q = oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터 입니다 : [%03d]", i);
+			String content = "내용 무";
+			this.questionService.create(subject, content);
 		}
-	}
-
-	@Test
-	@DisplayName("findBySubject")
-	void t4() {
-		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-		assertEquals(1, q.getId());
-	}
-
-	@Test
-	@DisplayName("findBySubjectAndContent")
-	void t5() {
-		Question q = this.questionRepository.findBySubjectAndContent(
-				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-		assertEquals(1, q.getId());
-	}
-
-	@Test
-	@DisplayName("findBySubjectLike")
-	void t6() {
-		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
-		Question q = qList.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
-	}
-
-	@Test
-	@DisplayName("update")
-	void t7() {
-		Optional<Question> oq = this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-
-		Question q = oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
-	}
-
-	@Test
-	@DisplayName("delete")
-	void t8() {
-		assertEquals(2, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(1);
-		assertTrue(oq.isPresent());
-
-		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1, this.questionRepository.count());
-	}
-
-	@Test
-	@DisplayName("insert A")
-	void t9() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
-	}
-
-	@Test
-	@DisplayName("findById A")
-	void t10() {
-		Optional<Answer> oa = this.answerRepository.findById(1);
-		assertTrue(oa.isPresent());
-		Answer a = oa.get();
-		assertEquals(2, a.getQuestion().getId());
-	}
-
-	// Transactional을 통해 해당 문제 해결
-	@Transactional
-	@Test
-	@DisplayName("findByWhat")
-	void t11() {
-		Optional<Question> oq = this.questionRepository.findById(2);
-		// 이후에 DB 세션이 끊어져 이후에 오류 발생
-		assertTrue(oq.isPresent());
-		Question q = oq.get();
-
-		// 아래 코드에서 오류 발생
-		List<Answer> answerList = q.getAnswerList();
-
-		assertEquals(1, answerList.size());
-		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 	}
 }
